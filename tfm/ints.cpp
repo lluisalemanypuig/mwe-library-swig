8719dae110e2a5fc92c3d450b4cd7efb19914972
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-10-14 08:54:41 +0200

    Use OpenMP

--- tfm/ints.cpp
+++ tfm/ints.cpp
@@ -4,4 +4,14 @@ namespace tfm {
 
 int return_0() noexcept { return 0; }
 
+int uses_omp() noexcept {
+	int s = 0;
+	
+	#pragma omp parallel for reduction(+:s)
+	for (int i = 0; i < 10000; ++i) {
+	}
+	
+	return s;
+}
+
 }
--- tfm/test_for_mac.hpp
+++ tfm/test_for_mac.hpp
@@ -9,4 +9,6 @@ std::string return_goodbye_world() noexcept;
 
 int return_0() noexcept;
 
+int uses_omp() noexcept;
+
 } // -- namespace tfm
