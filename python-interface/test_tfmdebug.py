d5316ec8e0bacf436054fe2e526a4d6ff0b4e14a
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-10-14 08:27:02 +0200

    Added python interface

---
+++ python-interface/test_tfmdebug.py
@@ -0,0 +1,5 @@
+import tfmdebug
+
+print(tfmdebug.return_hello_world())
+print(tfmdebug.return_goodbye_world())
+print(tfmdebug.return_0())
---
+++ python-interface/tfmdebug.i
@@ -0,0 +1,9 @@
+%module tfmdebug
+
+%include std_string.i
+
+%{
+#include <tfm/test_for_mac.hpp>
+%}
+
+%include "../tfm/test_for_mac.hpp"
